1464507cb54d1b1184ac2b12943819344c652099
HenriqueKrause <henriquebkrause2312@gmail.com>
2023-03-13 10:07:51 -0300

    commits to class my graduation

--- class/exercicio10.c
+++ class/exercicio10.c
@@ -2,44 +2,35 @@
 // linhas e m colunas. Após, para cada linha da matriz, o algoritmo calcula a média e conta
 // quantos elementos estão acima da média calculada, imprimindo estas informações
 
-#include <stdio.h> 
+#include <stdio.h>
 
+int main() {
+    int n, m;
+    printf("Digite o número de linhas (n) e colunas (m): ");
+    scanf("%d%d", &n, &m);
 
-int main(){
-    int m,n;
     double matriz[n][m];
-
-    printf("Digite o numero de linhas da matriz:");
-    scanf("%d", &n);
-
-    printf("Digite o numero de colunas da matriz:");
-    scanf("%d", &m);
-
-for(int i =0; i<n; i++){
-    for(int j=0; j<m; j++){
-        printf("Digite o elemento da linha %d e coluna %d:", i, j);
-        scanf("%lf", &matriz[i][j]);
-    }
-
-    double soma = 0;
-    for(int j=0; j<m; j++){
-        soma += matriz[i][j];
-    }
-
-    double media = soma/m;
-
-    int acima_media = 0;
-    for(int j=0; j<m; j++){
-        if(matriz[i][j] > media){
-            acima_media++;
+    for (int i = 0; i < n; i++) {
+        printf("Digite os %d valores da linha %d: ", m, i+1);
+        for (int j = 0; j < m; j++) {
+            scanf("%lf", &matriz[i][j]);
         }
     }
 
-    printf("A média da linha %d é %lf e tem %d elementos acima da média\n", i, media, acima_media);
+    for (int i = 0; i < n; i++) {
+        double soma = 0;
+        for (int j = 0; j < m; j++) {
+            soma += matriz[i][j];
+        }
+        double media = soma / m;
+        int acima_da_media = 0;
+        for (int j = 0; j < m; j++) {
+            if (matriz[i][j] > media) {
+                acima_da_media++;
+            }
+        }
+        printf("Linha %d: media = %.2lf, %d elementos acima da media\n", i+1, media, acima_da_media);
+    }
 
     return 0;
-
-}
-
-
 }
