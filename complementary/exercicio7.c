6ea133ad4636df010c7d7be6a42b7b83a28e60b8
HenriqueKrause <henriquebkrause2312@gmail.com>
2023-03-15 11:02:32 -0300

    commits to class my graduation

---
+++ complementary/exercicio7.c
@@ -0,0 +1,17 @@
+//iniciando funções 
+
+
+#include <stdio.h>
+
+void separador(){
+    printf("|-----------------------------------|\n");
+}
+
+int main(){
+
+    separador();
+    printf("Hello world\n");
+    separador();
+
+    return 0;
+}
